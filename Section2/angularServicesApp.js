3bc04a80cbbab68fa6fb06a632734460c75e3ccc
Randy Davis <randy_davis@outlook.com>
2015-10-04 14:20:17 -0500

    begin Section 3 and complete routing fundamentals example; update section2 files to use best practices for module declaration

--- Section2/angularServicesApp.js
+++ Section2/angularServicesApp.js
@@ -1,24 +1,16 @@
 /*** Created by randy on 10/4/15.*/
-var myApp = angular.module('myFirstApp', []);
+angular.module('myFirstApp', [])
 
-myApp.factory('personService', function() {
+    .factory('personService', function() {
 
-    var person = {};
+        var person = {};
 
-    person.printName = function(firstName, lastName) {
-        return firstName + ' ' + lastName;
-    }
+        person.printName = function(firstName, lastName) {
+            return firstName + ' ' + lastName;
+        }
 
-    return person;
-})
+        return person;
+    })
 
 
-myApp.controller('myController', function($scope, personService) {
-    $scope.firstName = "Trainee";
-    $scope.lastName = "Davis";
 
-    $scope.printName = function() {
-        return personService.printName($scope.firstName, $scope.lastName);
-    }
-
-})
---
+++ Section3/app.js
@@ -0,0 +1,22 @@
+angular.module('myFirstApp', ['ngRoute'])
+
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+			.when('/about', {
+				templateUrl: 'views/about.html'
+			})
+			.when('/contact', {
+				templateUrl: 'views/contact.html'
+			})
+			.otherwise({redirectTo: '/'})
+	}])
+
+	.factory('personService',  function(){
+
+		 var person = {};
+
+		 person.printName = function(firstName,lastName) {
+			return firstName + ' ' +lastName;
+		 }
+		return person;
+	})
