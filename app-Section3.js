7eab4842bc86a99cd2d4883de85ba91573efdc42
Randy Davis <randy_davis@outlook.com>
2015-10-05 23:46:16 -0500

    finish route controllers and $routeParams section

--- app-Section3.js
+++ app-Section3.js
@@ -1,12 +1,16 @@
 angular.module('myFirstApp', ['ngRoute'])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-	   $routeProvider.when('/about', {
-	   	 templateUrl:'views/about.html'
-	   })
-	   .when('/contact', {
-	   	templateUrl:'views/contact.html'
-	   })
-	   .otherwise({redirectTo:'/'})
+	   $routeProvider
+           .when('/about/:param1', {
+               //templateUrl:'views/about.html', // Use this one vs template alone
+               template: '<h1>I AM THE ABOUT PAGE</h1>',
+               controller: 'AboutCtrl'
+           })
+           .when('/contact/:param1', {
+                templateUrl:'views/contact.html',
+                controller: 'ContactCtrl'
+           })
+           .otherwise({redirectTo:'/'})
 
 	   $locationProvider.html5Mode(true);
 
--- controller-Section3.js
+++ controller-Section3.js
@@ -5,8 +5,16 @@ angular.module('myFirstApp')
     $scope.lastName = 'Davis';
      
 
-    $scope.printName = function () { return personService.printName($scope.firstName,$scope.lastName) }
- ;
+    $scope.printName = function () { return personService.printName($scope.firstName,$scope.lastName) };
 
+} )
 
-} );
+.controller('AboutCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+
+        $scope.input = $routeParams.param1
+
+}])
+
+.controller('ContactCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+        $scope.input = $routeParams.param1
+}])
